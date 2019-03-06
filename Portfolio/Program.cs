124c58642df89a16524353df574ef6234f8e9331
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-03-05 21:22:23 -0500

    Add https_port

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -19,7 +19,7 @@ namespace Portfolio
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseSetting("https_port", "5000")
+                .UseSetting("https_port", Environment.GetEnvironmentVariable("PORT"))
                 .UseStartup<Startup>();
     }
 }
