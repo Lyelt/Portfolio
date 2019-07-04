b95ff233b78cfdb1fe56023274cc9a3b4ae514d0
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-07-04 01:17:29 -0400

    Add initial login page

--- Portfolio/ClientApp/src/app/auth/auth-guard.service.ts
+++ Portfolio/ClientApp/src/app/auth/auth-guard.service.ts
@@ -1,17 +1,17 @@
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { Router, CanActivate } from '@angular/router';
+import { Router, CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 
 export class AuthGuard implements CanActivate {
   constructor(private jwtHelper: JwtHelperService, private router: Router) {
   }
 
-  canActivate() {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     var token = localStorage.getItem("jwt");
 
     if (token && !this.jwtHelper.isTokenExpired(token)) {
       return true;
     }
-    this.router.navigate(["login"]);
+    this.router.navigate(["login"], { queryParams: { returnUrl: state.url } });
     return false;
   }
 }
