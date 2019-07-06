b645586d2161da7d4b69f279e2f5162d3329c7df
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-07-05 23:51:27 -0400

    Revert temporary debug code

--- Portfolio/Controllers/AuthController.cs
+++ Portfolio/Controllers/AuthController.cs
@@ -33,7 +33,6 @@ namespace Portfolio.Controllers
         [Route("Auth/Login")]
         public IActionResult Login([FromBody]Credentials credentials)
         {
-            return Unauthorized(); // TEMPORARY
             if (credentials?.Username == null || credentials?.Password == null)
                 return BadRequest("Invalid client request");
 
