94890eac540ffe2e3aca2e25b462feda3983e526
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-07-12 16:25:08 -0400

    Remove UseKestrel

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -19,10 +19,10 @@ namespace Portfolio
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseKestrel(options =>
-                {
-                    options.Listen(System.Net.IPAddress.Loopback, int.Parse(Environment.GetEnvironmentVariable("PORT")));
-                });
+                .UseStartup<Startup>();
+                //.UseKestrel(options =>
+                //{
+                //    options.Listen(System.Net.IPAddress.Loopback, int.Parse(Environment.GetEnvironmentVariable("PORT")));
+                //});
     }
 }
