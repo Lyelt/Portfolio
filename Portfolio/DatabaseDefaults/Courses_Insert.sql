369d2c8dcb5c082763086f1e0ebfcceca5374eb4
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-07-15 19:55:39 -0400

    Fix empty parenthesis appearing after star name.
    Reduce space between courses.
    
    Add "Categories" to db inserts

--- Portfolio/DatabaseDefaults/Courses_Insert.sql
+++ Portfolio/DatabaseDefaults/Courses_Insert.sql
@@ -4,28 +4,30 @@ LIMIT 0, 1000
 
 -- Date: 2019-07-15 09:45
 */
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (1,'Bob-omb Battlefield','BoB');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (2,'Whomp\'s Fortress','WF');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (3,'Jolly Roger Bay','JRB');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (4,'Cool, Cool Mountain','CCM');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (5,'Big Boo\'s Haunt','BBH');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (6,'Hazy Maze Cave','HMC');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (7,'Lethal Lava Land','LLL');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (8,'Shifting Sand Lane','SSL');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (9,'Dire, Dire Docks','DDD');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (10,'Snowman\'s Land','SL');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (11,'Wet-Dry World','WDW');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (12,'Tall, Tall Mountain','TTM');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (13,'Tiny-Huge Island','THI');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (14,'Tick Tock Clock','TTC');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (15,'Rainbow Ride','RR');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (16,'Princess\'s Secret Slide','Slide');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (17,'Aquarium','');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (18,'Bowser in the Dark World','BitDW');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (19,'Wing Cap','');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (20,'Metal Cap','');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (21,'Vanish Cap','');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (22,'Bowser in the Fire Sea','BitFS');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (23,'Cloud Stage','');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (24,'Bowser in the Sky','BitS');
-INSERT INTO `` (`CourseId`,`Name`,`Abbreviation`) VALUES (25,'Castle','');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (1,'Bob-omb Battlefield','BoB');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (2,'Whomp\'s Fortress','WF');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (3,'Jolly Roger Bay','JRB');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (4,'Cool, Cool Mountain','CCM');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (5,'Big Boo\'s Haunt','BBH');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (6,'Hazy Maze Cave','HMC');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (7,'Lethal Lava Land','LLL');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (8,'Shifting Sand Lane','SSL');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (9,'Dire, Dire Docks','DDD');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (10,'Snowman\'s Land','SL');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (11,'Wet-Dry World','WDW');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (12,'Tall, Tall Mountain','TTM');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (13,'Tiny-Huge Island','THI');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (14,'Tick Tock Clock','TTC');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (15,'Rainbow Ride','RR');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (16,'Princess\'s Secret Slide','Slide');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (17,'Aquarium','');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (18,'Bowser in the Dark World','BitDW');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (19,'Wing Cap','');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (20,'Metal Cap','');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (21,'Vanish Cap','');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (22,'Bowser in the Fire Sea','BitFS');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (23,'Cloud Stage','');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (24,'Bowser in the Sky','BitS');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (25,'Castle','');
+INSERT INTO `portfolioDB`.`Courses` (`CourseId`,`Name`,`Abbreviation`) VALUES (26,'Categories','');
+
