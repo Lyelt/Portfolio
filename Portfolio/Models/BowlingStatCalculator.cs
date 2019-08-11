9c6e6b71bd82fa925c0831ba3834b4a14c502794
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-08-11 12:46:48 -0400

    Add personal records for best/worst scores
    Add strike percentage

--- Portfolio/Models/BowlingStatCalculator.cs
+++ Portfolio/Models/BowlingStatCalculator.cs
@@ -18,11 +18,11 @@ namespace Portfolio.Models
         public List<BowlingStat> GetOverallStats()
         {
             var stats = new List<BowlingStat>();
-            stats.Add(TotalGamesBowled());
             stats.Add(OverallAverage());
             stats.Add(HighestScore());
+            stats.Add(TotalGamesBowled());
+            stats.Add(StrikePercentage());
             stats.Add(SinglePinSpares());
-            stats.Add(BestSessionAverage());
             return stats;
         }
 
@@ -32,11 +32,31 @@ namespace Portfolio.Models
 
         public BowlingStat HighestScore() => new BowlingStat("Highest Score", _games.Select(g => g.TotalScore).DefaultIfEmpty(0).Max());
 
+        public BowlingStat LowestScore() => new BowlingStat("Lowest Score", _games.Select(g => g.TotalScore).DefaultIfEmpty(0).Min());
+
         public BowlingStat BestSessionAverage()
         {
             var gamesBySession = _games.GroupBy(g => g.Session).Select(g => new { Session = g.Key, Games = g.ToList() });
             var bestSession = gamesBySession.MaxBy(gs => AverageOfGames(gs.Games)).FirstOrDefault();
-            return new BowlingStat("Best Session Average", AverageOfGames(bestSession.Games), details: $"Session {bestSession.Session.Id} on {bestSession.Session.Date.ToShortDateString()}");
+            return bestSession != null ? 
+                new BowlingStat("Best Session Average", AverageOfGames(bestSession.Games), details: $"Session {bestSession.Session.Id} on {bestSession.Session.Date.ToShortDateString()}")
+              : new BowlingStat("Best Session Average", 0);
+        }
+        public BowlingStat WorstSessionAverage()
+        {
+            var gamesBySession = _games.GroupBy(g => g.Session).Select(g => new { Session = g.Key, Games = g.ToList() });
+            var worstSession = gamesBySession.MinBy(gs => AverageOfGames(gs.Games)).FirstOrDefault();
+            return worstSession != null ? 
+                new BowlingStat("Worst Session Average", AverageOfGames(worstSession.Games), details: $"Session {worstSession.Session.Id} on {worstSession.Session.Date.ToShortDateString()}")
+              : new BowlingStat("Worst Session Average", 0);
+        }
+
+        public BowlingStat StrikePercentage()
+        {
+            var allFrames = _games.SelectMany(g => g.Frames).ToList();
+            var strikes = allFrames.Where(f => f.IsStrike()).ToList();
+            double ratio = allFrames.Count > 0 ? ((double)strikes.Count / allFrames.Count) : 0;
+            return new BowlingStat("Strike Percentage", Math.Round(ratio * 100, 2), "%", $"{strikes.Count}/{allFrames.Count}");
         }
 
         public BowlingStat SinglePinSpares()
@@ -59,7 +79,7 @@ namespace Portfolio.Models
             foreach (var group in countGroups.OrderByDescending(g => g.CountValue))
             {
                 double ratio = allFrames.Count > 0 ? ((double)group.Number / allFrames.Count) : 0;
-                counts.Add(new BowlingStat($"Number of {group.CountValue} counts", group.Number, details: $"{Math.Round(ratio * 100, 2)}%"));
+                counts.Add(new BowlingStat($"{group.CountValue} counts", Math.Round(ratio * 100, 2), "%", $"{group.Number}/{allFrames.Count}"));
             }
 
             return counts;
@@ -77,6 +97,16 @@ namespace Portfolio.Models
             splits.Add(new BowlingStat("Split Conversions", Math.Round(ratio * 100, 2), "%", $"{convertedSplits.Count}/{splitFrames.Count}"));
 
             return splits;
+        }
+
+        public List<BowlingStat> GetRecords()
+        {
+            var records = new List<BowlingStat>();
+            records.Add(HighestScore());
+            records.Add(LowestScore());
+            records.Add(BestSessionAverage());
+            records.Add(WorstSessionAverage());
+            return records;
         }
 
         private double AverageOfGames(List<BowlingGame> games) => Math.Round(games.Select(g => g.TotalScore).DefaultIfEmpty(0).Average(), 2);
