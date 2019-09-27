6030858a7125fb6db09c8eeac338fac1a1e867a0
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-09-26 23:52:49 -0400

    Continued work on redesigning the bowling page.
    Convert more classes to tailwind, and add new custom button styles.

--- Portfolio/ClientApp/tailwind.config.js
+++ Portfolio/ClientApp/tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
     }
   },
   variants: {
-    borderColor: ['responsive', 'hover', 'focus', 'active', 'group-hover']
+    borderColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'odd', 'even']
   },
   plugins: [
     require('@tailwindcss/custom-forms'),
