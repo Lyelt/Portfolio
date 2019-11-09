abd95e3cd41b840c3a892bb1350cb24b34e26d62
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-11-08 22:21:18 -0500

    Add ability to remove entries from the chart.

--- Portfolio/ClientApp/src/app/bowling/components/bowling/bowling.component.ts
+++ Portfolio/ClientApp/src/app/bowling/components/bowling/bowling.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
 import { MatDialogConfig, MatDialog } from '@angular/material';
 import { BowlingStartSessionComponent } from '../bowling-start-session/bowling-start-session.component';
-import { BowlingSession } from '../../models/bowling-session';
-import { BowlingService } from '../../services/bowling.service';
 import { BowlingChartComponent } from '../bowling-chart/bowling-chart.component';
 import { SeriesCategory, SeriesCategoryEnum } from '../../models/series-category';
 import { BowlingUtilities } from '../../models/bowling-utilities';
@@ -19,19 +16,19 @@ export class BowlingComponent implements OnInit {
   currentSeriesCategory: SeriesCategory;
   categoryLabels: SeriesCategory[] = BowlingUtilities.lineSeriesCategories;
 
-  allSessions: BowlingSession[] = [];
-
   selectedStartTime: Date;
   selectedEndTime: Date;
 
-  constructor(private bowlingService: BowlingService,
-    private router: Router,
-    private dialog: MatDialog) {
+  constructor(private dialog: MatDialog) {
 
   }
 
   ngOnInit() {
     this.currentSeriesCategory = this.categoryLabels.find(c => c.category == SeriesCategoryEnum.SessionAverage);
+  }
+
+  refreshChart() {
+    this.chart.loadSeriesData();
   }
 
   updateStats() {
@@ -51,6 +48,6 @@ export class BowlingComponent implements OnInit {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
 
-    const dialogRef = this.dialog.open(BowlingStartSessionComponent, dialogConfig);
+    this.dialog.open(BowlingStartSessionComponent, dialogConfig);
   }
 }
