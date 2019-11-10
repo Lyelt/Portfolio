ffc6d8b25e0679938fa701b7305e4b7b74ee0214
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-11-10 13:27:40 -0500

    Add the ability to open a session via the chart

--- Portfolio/ClientApp/src/app/bowling/components/bowling/bowling.component.ts
+++ Portfolio/ClientApp/src/app/bowling/components/bowling/bowling.component.ts
@@ -6,49 +6,50 @@ import { SeriesCategory, SeriesCategoryEnum } from '../../models/series-category
 import { BowlingUtilities } from '../../models/bowling-utilities';
 
 @Component({
-  selector: 'app-bowling',
-  templateUrl: './bowling.component.html',
-  styleUrls: ['./bowling.component.scss']
+    selector: 'app-bowling',
+    templateUrl: './bowling.component.html',
+    styleUrls: ['./bowling.component.scss']
 })
 export class BowlingComponent implements OnInit {
-  @ViewChild('chart') chart: BowlingChartComponent;
-  currentUserId: string = localStorage.getItem("userId");
-  currentSeriesCategory: SeriesCategory;
-  categoryLabels: SeriesCategory[] = BowlingUtilities.lineSeriesCategories;
+    @ViewChild('chart') chart: BowlingChartComponent;
+    currentUserId: string = localStorage.getItem("userId");
+    currentSeriesCategory: SeriesCategory;
+    categoryLabels: SeriesCategory[] = BowlingUtilities.lineSeriesCategories;
 
-  selectedStartTime: Date;
-  selectedEndTime: Date;
+    selectedStartTime: Date;
+    selectedEndTime: Date;
 
-  constructor(private dialog: MatDialog) {
+    constructor(private dialog: MatDialog) {
 
-  }
+    }
 
-  ngOnInit() {
-    this.currentSeriesCategory = this.categoryLabels.find(c => c.category == SeriesCategoryEnum.SessionAverage);
-  }
+    ngOnInit() {
+        this.currentSeriesCategory = this.categoryLabels.find(c => c.category == SeriesCategoryEnum.SessionAverage);
+    }
 
-  refreshChart() {
-    this.chart.loadSeriesData();
-  }
+    refreshChart() {
+        this.chart.loadSeriesData();
+    }
 
-  updateStats() {
-    this.selectedStartTime = this.chart.getStartTime();
-    this.selectedEndTime = this.chart.getEndTime();
-    this.chart.loadSeriesDataWithTimeRange();
-  }
+    updateStats() {
+        this.selectedStartTime = this.chart.getStartTime();
+        this.selectedEndTime = this.chart.getEndTime();
+        this.chart.loadSeriesDataWithTimeRange();
+    }
 
-  selectUser(userId: string) {
-    this.currentUserId = userId;
-  }
+    selectUser(userId: string) {
+        this.currentUserId = userId;
+    }
 
-  selectSeriesCategory(category: SeriesCategory) {
-    this.currentSeriesCategory = category;
-  }
+    selectSeriesCategory(category: SeriesCategory) {
+        this.currentSeriesCategory = category;
+    }
 
-  openDialog() {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.autoFocus = true;
+    openDialog(dataPoint = null) {
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.autoFocus = true;
+        dialogConfig.data = dataPoint;
 
-    this.dialog.open(BowlingStartSessionComponent, dialogConfig);
-  }
+        this.dialog.open(BowlingStartSessionComponent, dialogConfig);
+    }
 }
