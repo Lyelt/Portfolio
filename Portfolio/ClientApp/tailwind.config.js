56287c0318e3b92e4c473678a38040da00ea5725
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-11-22 01:06:07 -0500

    Fix up hover and focus styles and change sort icon

--- Portfolio/ClientApp/tailwind.config.js
+++ Portfolio/ClientApp/tailwind.config.js
@@ -1,24 +1,26 @@
 module.exports = {
-  theme: {
-    extend: {
-      zIndex: {
-        '999': 999
-      },
-      flexGrow: {
-        '1/2': 1/2
-      },
-      fontSize: {
-        '2xs': '.5rem',
-        '3xs': '.25rem'
-      }
-    }
-  },
-  variants: {
-    borderColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
-    backgroundColor: ['responsive', 'hover', 'focus', 'odd', 'even']
-  },
-  plugins: [
-    require('@tailwindcss/custom-forms'),
-    require('glhd-tailwindcss-transitions')()
-  ]
+    theme: {
+        extend: {
+            zIndex: {
+                '999': 999
+            },
+            flexGrow: {
+                '1/2': 1 / 2
+            },
+            fontSize: {
+                '2xs': '.5rem',
+                '3xs': '.25rem'
+            }
+        }
+    },
+    variants: {
+        borderWidth: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+        borderStyle: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+        borderColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+        backgroundColor: ['responsive', 'hover', 'focus', 'odd', 'even']
+    },
+    plugins: [
+        require('@tailwindcss/custom-forms'),
+        require('glhd-tailwindcss-transitions')()
+    ]
 };
