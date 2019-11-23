34e79f642ce9fdf1e790aeaa9a2ad0cffc491873
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-11-23 16:53:49 -0500

    Add animations to players when switching turn.
    Show an arrow as a button instead of "next turn" link.

--- Portfolio/ClientApp/src/app/turn-tracker/components/player/player.component.ts
+++ Portfolio/ClientApp/src/app/turn-tracker/components/player/player.component.ts
@@ -1,10 +1,37 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Player } from '../../models/player'
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+    styleUrls: ['./player.component.scss'],
+    animations: [
+        trigger(
+            'slideButton', [
+            transition(':enter', [
+                style({ transform: 'translateX(-50%)', opacity: 0 }),
+                animate('500ms', style({ transform: 'translateX(0)', opacity: 1 }))
+            ]),
+            transition(':leave', [
+                style({ transform: 'translateX(0%)', opacity: 1 }),
+                animate('500ms', style({ transform: 'translateX(50%)', opacity: 0 }))
+            ]),
+        ]
+        ),
+        trigger(
+            'fade', [
+            transition(':enter', [
+                style({ opacity: 0 }),
+                animate('100ms 500ms', style({ opacity: 1 }))
+            ]),
+            //transition(':leave', [
+            //    style({ opacity: 1 }),
+            //    animate('100ms 1000ms', style({ opacity: 0 }))
+            //]),
+        ]
+        )
+    ]
 })
 export class PlayerComponent implements OnInit {
 
