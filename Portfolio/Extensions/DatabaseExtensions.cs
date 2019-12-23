10c0bdf7a96151f757180b4a603a7b6b3f99cebf
Nicholas Ghobrial <nicholasghob@yahoo.com>
2019-12-23 17:46:18 -0500

    Test only populating autcomplete options on search

---
+++ Portfolio/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Portfolio.Data;
+using Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static IServiceCollection ConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connStr = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? "Server=127.0.0.1;Database=portfolio;Uid=root;Pwd=Dolphin92Dslshst;";
+
+            services
+                .AddDbContext<PortfolioContext>(options => options.UseMySql(connStr))
+                .AddDbContext<SpeedrunContext>(options => options.UseMySql(connStr))
+                .AddDbContext<BowlingContext>(options => options.UseMySql(connStr))
+                .AddDbContext<YugiohContext>(options => options.UseMySql(connStr));
+
+            return services;
+        }
+    }
+}
