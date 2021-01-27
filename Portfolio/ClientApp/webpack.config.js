f0d3386df4e9c4b25db0ec5d5137353490e23426
Nicholas Ghobrial <nicholasghob@yahoo.com>
2021-01-27 10:18:13 -0500

    Add logging to purge config

--- Portfolio/ClientApp/webpack.config.js
+++ Portfolio/ClientApp/webpack.config.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 module.exports = (config) => {
   const purgeEnv = process.env.ENABLE_PURGE;
   tailwindConfig.purge = purgeEnv === null || purgeEnv === 'true';
+  console.log("Tailwind purging CSS: " + tailwindConfig.purge);
   patchPostCSS(config, tailwindConfig);
   return config;
 };
