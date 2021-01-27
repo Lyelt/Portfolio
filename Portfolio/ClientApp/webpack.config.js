047dc37e57b723b549219f1a484010d69ff01a4a
Nicholas Ghobrial <nicholasghob@yahoo.com>
2021-01-27 14:25:45 -0500

    Clean up purge config

--- Portfolio/ClientApp/webpack.config.js
+++ Portfolio/ClientApp/webpack.config.js
@@ -3,9 +3,9 @@ const tailwindConfig = require("./tailwind.config.js");
 require('dotenv').config();
 
 module.exports = (config) => {
-  const purgeEnv = process.env.ENABLE_PURGE || null;
-  tailwindConfig.purge.enabled = (purgeEnv === null || purgeEnv === 'true');
-  console.log("dotenv ENABLE_PURGE variable: " + purgeEnv + " --- Tailwind purging CSS: " + tailwindConfig.purge.enabled);
+  const purgeEnv = process.env.ENABLE_PURGE;
+  tailwindConfig.purge.enabled = !purgeEnv || purgeEnv === 'true';
+  console.log("tailwindConfig.purge.enabled=" + tailwindConfig.purge.enabled);
   patchPostCSS(config, tailwindConfig);
   return config;
 };
