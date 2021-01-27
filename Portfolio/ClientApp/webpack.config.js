4ec085bec444e49a5ef0631c15dca16e94b082be
Nicholas Ghobrial <nicholasghob@yahoo.com>
2021-01-27 10:46:52 -0500

    Fix purge config

--- Portfolio/ClientApp/webpack.config.js
+++ Portfolio/ClientApp/webpack.config.js
@@ -3,7 +3,7 @@ const tailwindConfig = require("./tailwind.config.js");
 require('dotenv').config();
 
 module.exports = (config) => {
-  const purgeEnv = process.env.ENABLE_PURGE;
+  const purgeEnv = process.env.ENABLE_PURGE || null;
   tailwindConfig.purge = purgeEnv === null || purgeEnv === 'true';
   console.log("dotenv ENABLE_PURGE variable: " + purgeEnv + " --- Tailwind purging CSS: " + tailwindConfig.purge);
   patchPostCSS(config, tailwindConfig);
