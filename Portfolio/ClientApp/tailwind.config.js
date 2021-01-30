de2c4900f7e526ba408a17e267f448961e0572a1
Nicholas Ghobrial <nicholasghob@yahoo.com>
2021-01-29 22:05:42 -0500

    Add some subtle animations to various components
    Fix up some colors and spacing on home page

--- Portfolio/ClientApp/tailwind.config.js
+++ Portfolio/ClientApp/tailwind.config.js
@@ -20,7 +20,8 @@ module.exports = {
         black: colors.black,
         red: colors.red,
         green: colors.green,
-        yellow: colors.yellow
+        yellow: colors.yellow,
+        blue: colors.blue
       },
       extend: {
         zIndex: {
@@ -32,18 +33,27 @@ module.exports = {
         fontSize: {
           '2xs': '.5rem',
           '3xs': '.25rem'
+        },
+        transitionProperty: {
+          'bg': 'background-color',
+          'center': 'left, right',
         }
       },
      
     },
     variants: {
-      borderWidth: ['responsive', 'hover', 'focus', 'active', 'group-hover', 'last', 'first'],
-      borderStyle: ['responsive', 'hover', 'focus', 'active', 'group-hover', 'last', 'first'],
-      borderColor: ['responsive', 'hover', 'focus', 'active', 'group-hover', 'last', 'first', 'dark'],
-      backgroundColor: ['responsive', 'hover', 'focus', 'odd', 'even', 'dark'],
-      margin: ['responsive', 'first', 'last'],
-      opacity: ({after})=>after(['disabled']),
-      pointerEvents: ({after}) => after(['disabled'])
+      extend: {
+        borderWidth: ['responsive', 'hover', 'focus', 'active', 'group-hover', 'last', 'first'],
+        borderStyle: ['responsive', 'hover', 'focus', 'active', 'group-hover', 'last', 'first'],
+        borderColor: ['responsive', 'hover', 'focus', 'active', 'group-hover', 'last', 'first', 'dark'],
+        backgroundColor: ['responsive', 'hover', 'focus', 'odd', 'even', 'dark'],
+        margin: ['responsive', 'first', 'last'],
+        transitionProperty: ['hover', 'focus'],
+        transitionTimingFunction: ['hover', 'focus'],
+        transitionDuration: ['hover', 'focus'],
+        opacity: ['disabled'],
+        pointerEvents: ['disabled'],
+      }
     },
   plugins: [
     require('@tailwindcss/forms')
