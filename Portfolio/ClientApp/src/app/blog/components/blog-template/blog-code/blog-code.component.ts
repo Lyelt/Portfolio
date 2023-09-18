06a93d09f07ae582381ec353b38f2aa7db046a18
Nicholas Ghobrial <nicholasghob@yahoo.com>
2023-09-18 15:17:19 -0400

    September 2023 Blog Article

---
+++ Portfolio/ClientApp/src/app/blog/components/blog-template/blog-code/blog-code.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog-code',
+  templateUrl: './blog-code.component.html',
+  styleUrls: ['./blog-code.component.scss']
+})
+export class BlogCodeComponent {
+
+}
